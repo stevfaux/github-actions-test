c9c5ac529c9de28f4c499c080f27e45c0e0bcdba
stevfaux <red_faustino25@yahoo.com>
2021-04-30 09:58:45 +0800

    first workflow commit

---
+++ Sample.groovy
@@ -0,0 +1,20 @@
+/*def name = "red"
+def nickname = "white"
+print "Name is:${name}"
+System.out.println("\t")
+print "Nickname is:${nickname}"*/
+
+/*
+class MyFirstGroovyProgram {
+        static void main(args) {
+        print('Hello World!.!');
+    }
+
+
+
+}*/
+
+def x = 2
+def y = 5
+
+println("The sum is:${x + y}")
